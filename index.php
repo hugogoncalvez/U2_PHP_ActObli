473dc217a0c2ea43723cf4c0064806fe44751cfd
hugogoncalvez <hugogoncalvez@gmail.com>
2021-07-04 14:22:03 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+$nombre = 'Hugo Gabriel';
+$apellido= 'Goncalvez';
+$edad = 48;
+$hobbye = 'Aeromodelismo RC';
+$editorCodigo = 'Visual Studio Code';
+$sistOperativo = 'Windos 10 64 bits';
+
+echo('<h2> Nombre: '.$nombre.'</h2>');
+echo('<h2> Apellido: '.$apellido.'</h2>');
+echo('<h2> Edad: '.$edad.'</h2>');
+echo('<h2> Hobbye: '.$hobbye.'</h2>');
+echo('<h2> Editor de Código: '.$editorCodigo.'</h2>');
+echo('<h2> Sistema Operativo: '.$sistOperativo.'</h2>');
+?>
